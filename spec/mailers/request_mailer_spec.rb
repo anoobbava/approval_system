b042bf9f154ad49abe1fb73beb7f449debc550c6
Anoob Bava <anoob.bava@gmail.com>
2017-10-30 23:08:13 +0530

    completed the spec for request mailers

--- spec/mailers/request_mailer_spec.rb
+++ spec/mailers/request_mailer_spec.rb
@@ -1,5 +1,22 @@
-require "rails_helper"
+require 'spec_helper'
 
 RSpec.describe RequestMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:all) do
+    ActionMailer::Base.delivery_method = :test
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.deliveries = []
+    @user = FactoryGirl.create(:user_login_valid)
+    @request = FactoryGirl.create(:request_approval_valid, requested_by: @user.id)
+  end
+
+  after(:all) do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  it 'checks request received or not' do
+    RequestMailer.request_received(@user, @request).deliver
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
+    expect(ActionMailer::Base.deliveries.first.subject).to eq('Your Request received')
+  end
 end
